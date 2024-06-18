efaa4ca6bb65cb6ee83f1cb6afec7be79015bd8c
ravvi-kumar <ravvi.kumar8989@gmail.com>
2024-06-18 23:30:15 +0530

    feat : add dev script

---
+++ dev.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+project=$1
+
+if [ $project == "react" ];then
+    cd react && npm i && npm run dev
+elif [ $project == "angular" ];then
+    cd angular && ng serve
+elif [ $project == "deno" ];then
+    cd deno && deno task start
+else 
+    echo "check you command again"
+fi
