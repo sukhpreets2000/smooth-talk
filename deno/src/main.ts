e5cf72ee77cec127877446188eac73f2a0e5ec1e
ravvi-kumar <ravvi.kumar8989@gmail.com>
2024-07-05 00:04:53 +0530

    fix : add lucia context middleware

--- deno/src/main.ts
+++ deno/src/main.ts
@@ -3,10 +3,32 @@ import { cors } from "hono/cors"
 
 import usersRoute from "./routes/users.route.ts";
 import authRoute from "./routes/auth.routes.ts";
+import { lucia } from "./db/lib/auth.ts";
+import { Context } from "./context.ts";
 
-const app = new Hono().basePath('/api')
+const app = new Hono<Context>().basePath('/api')
 
-app.use('/*', cors({ origin: '*' }))
+app.use('*', cors({ origin: '*' }))
+app.use("*", async (c, next) => {
+  const sessionId = lucia.readSessionCookie(c.req.header("Cookie") ?? "");
+  if (!sessionId) {
+    c.set("user", null);
+    c.set("session", null);
+    return next();
+  }
+
+  const { session, user } = await lucia.validateSession(sessionId);
+  if (session && session.fresh) {
+    c.header("Set-Cookie", lucia.createSessionCookie(session.id).serialize(), { append: true });
+  }
+  if (!session) {
+    c.header("Set-Cookie", lucia.createBlankSessionCookie().serialize(), { append: true });
+  }
+  c.set("session", session);
+  c.set("user", user);
+  return next();
+});
+
 app.get('/', (c) => {
   return c.text('Hello Hono!')
 })
